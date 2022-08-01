8afac589141d6de96f17ecc6817f6d9871a4e4c0
Felix <barosiofelix@gmail.com>
2022-08-01 20:24:00 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,21 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`
+}
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+function introductionWithLanguageOptional(name, language = 'JavaScript') {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+
+
+
+
+
+
+// console.log("Aki")
+// console.log("Aki", "Ember.js")
+// console.log("Gracie", "language")
